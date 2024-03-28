887b17d3c42394af8208b8718f1e6a623c25e770
babinicz <babinicz@onet.pl>
2024-03-28 16:01:39 +0100

    zm

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import ogPlugin from "vite-plugin-open-graph";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ogPlugin({
+      url: "https://example.com",
+      title: "Tytuł strony",
+      description: "Opis strony",
+      image: "https://marek-niedzwiecki.netlify.app/images/19.webp",
+      site_name: "Nazwa witryny",
+      locale: "pl_PL",
+    }),
+  ],
+});
