83751f2fd8a84c80455429008e45f11ac0af0afd
babinicz <babinicz@onet.pl>
2024-03-28 16:23:49 +0100

    zm

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import ogPlugin from "vite-plugin-open-graph";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ogPlugin({
-      url: "https://marek-niedzwiecki.netlify.app",
-      title: "Marek Niedźwiecki - Strona główna",
-      description:
-        "Oficjalna strona Marka Niedźwieckiego - Muzyk, Kompozytor, Autor tekstów",
-      image: "https://marek-niedzwiecki.netlify.app/images/19.webp",
-      site_name: "Marek Niedźwiecki",
-      type: "website", // Dodatkowe pole type
-      locale: "pl_PL",
-    }),
-  ],
+  plugins: [react()],
 });
