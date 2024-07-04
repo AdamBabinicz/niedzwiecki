c3716a97c4cba20b9b158c67193a3b3af20b8ec1
babinicz <babinicz@onet.pl>
2024-07-04 12:47:46 +0200

    przyciski w popup

--- src/GalleryPopup/GalleryPopup.jsx
+++ src/GalleryPopup/GalleryPopup.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FaChevronLeft, FaChevronRight, FaTimes } from "react-icons/fa"; // Ikony
 
 const GalleryPopup = ({ images, onClose }) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0); // Stan przechowujący indeks aktualnie wyświetlanego zdjęcia
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const imgRef = useRef(null);
 
   useEffect(() => {
     const handleKeyDown = (event) => {
@@ -32,6 +33,14 @@ const GalleryPopup = ({ images, onClose }) => {
     );
   };
 
+  useEffect(() => {
+    // Preload images
+    images.forEach((image, index) => {
+      const img = new Image();
+      img.src = image;
+    });
+  }, [images]);
+
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50">
       <div className="bg-white p-6 rounded-lg max-w-4xl relative">
@@ -43,12 +52,12 @@ const GalleryPopup = ({ images, onClose }) => {
         </button>
         <div className="relative">
           <img
+            ref={imgRef}
             src={images[currentImageIndex]}
             alt={`Image ${currentImageIndex + 1}`}
             className="w-full h-auto rounded-lg shadow-lg"
           />
         </div>
-        {/* Przyciski przewijania */}
         <button
           onClick={prevImage}
           className="absolute top-1/2 left-4 transform -translate-y-1/2 dark:bg-white bg-white p-2 rounded-full cursor-pointer dark:hover:bg-gray-200 dark:transition-colors hover:bg-gray-200 transition-colors focus:outline-none touch-none"
