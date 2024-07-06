b4e169e15c3b660dd43ee4d293d769d84bd34c8b
babinicz <babinicz@onet.pl>
2024-07-06 13:37:02 +0200

    zmiany

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -50,7 +50,7 @@ const Testimonials = () => {
             <Slider {...settings}>
               {testimonialData.map(({ id, name, text, img }, index) => {
                 return (
-                  <div key={index} className="my-6">
+                  <div key={index} className="mt-6">
                     {/* card */}
                     <div className="flex flex-col sm:flex-row gap-5 md:gap-14 p-4 mx-4 rounded-xl dark:bg-gray-800 relative">
                       <img
